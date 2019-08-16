da11999f2aa6cee17cef1aef7d8b6e29a7a4eb0b
lvorcool <wang.nlqf@163.com>
2019-08-16 17:26:45 +0800

    解析command input

---
+++ scripts/command.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+# @Time  : 2019-08-16 17:07
+# @Author: Xiawang
+# Description:
+
+
+class command:
+
+    def __init__(self, command_input):
+        self.command_input = command_input
+
+    def command_parse(self):
+        command_list = self.command_input.split(' ')
+        command_format = {}
+        for index, command in enumerate(command_list):
+            if command[:1] == '-':
+                command_format[command] = None
+                continue
+            command_format[command_list[index - 1]] = command
+        return command_format
---
+++ tests/test_command.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+# @Time  : 2019-08-16 17:05
+# @Author: Xiawang
+
+import pytest
+from scripts.command import command
+
+
+class Test_command:
+    def test_command_format(self, ):
+        command_input = '-l -p 8080 -d /usr/log'
+        assert command(command_input).command_parse() == {'-l': None, '-p': '8080', '-d': '/usr/log'}
