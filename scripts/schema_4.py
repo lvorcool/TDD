2dc547e56b8112e7b16c69f8a159993c20b0e441
lvorcool <wang.nlqf@163.com>
2019-09-02 21:04:32 +0800

    Default Changelist

---
+++ scripts/schema_4.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+# @Time  : 2019-09-02 21:03
+# @Author: Xiawang
+# Description:
+
+class Schema:
+
+    def get(self, flag):
+        if flag == 'l':
+            return 'bool'
---
+++ tests/test_schema_4.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+# @Time  : 2019-09-02 21:01
+# @Author: Xiawang
+
+import pytest
+from scripts.schema_4 import Schema
+
+class Test_test_schema_4:
+    def test_schema(self):
+        schema_text = 'l:bool p:int d:str'
+        assert Schema().get('l') == 'bool'
