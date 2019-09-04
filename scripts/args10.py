67d0a737ac303c78fcbd93dbbbc21c9808c64e04
lvorcool <wang.nlqf@163.com>
2019-09-04 20:15:12 +0800

    Default Changelist

---
+++ scripts/args10.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+# @Time  : 2019-09-04 20:11
+# @Author: Xiawang
+# Description:
+from scripts.command10 import Command10
+from scripts.schema10 import Schema10
+
+
+class Args10:
+
+    def __init__(self, schema_text, command_text):
+        self.schema = Schema10(schema_text)
+        self.command = Command10(command_text)
+
+    def get_value(self, flag):
+        return self.schema.get_value(self.command.get_value(flag))
---
+++ tests/test_args10.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+# @Time  : 2019-09-04 20:09
+# @Author: Xiawang
+
+import pytest
+from scripts.args10 import Args10
+
+
+class Test_test_args10:
+    def test_args(self, ):
+        schema_text = 'l:bool p:int d:str'
+        command_text = '-l true -p 8080 -d /usr/local'
+        Args10(schema_text, command_text).get_value('l') == True
